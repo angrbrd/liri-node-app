5c7a753bcc413d47ff4132fc12d3ed975b950f0e
Anna D Derbakova <hiopa16@gmail.com>
2017-02-21 00:12:08 -0500

    Adding Twitter consumer and access keys
    Adding the Twitter keys: consumer key/secret as well as access token
    key/secret.

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'mbg1foZtVoYsrIZ8MI6oASa9n',
+  consumer_secret: 'DUh47moCTCw3lx6ye3Gio9o2oOt8izsJUmdSPcQDj7sbkcdRU3',
+  access_token_key: '757763396972445696-IXzAYCtbkd9MiF7h9xEZjxdRxGq6i1P',
+  access_token_secret: 'GEaGG6XP0vXwd6pD3MAYERClJsRWsheEV4LRzhMRaRluA',
+}
